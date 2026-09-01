68bb65f909f91e506dd30984df9dec499c7a61e8
Diego <diegorodriguezmancini@gmail.com>
2026-09-01 23:01:40 +0200

    tweak some options, enable neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -7,13 +7,14 @@ vim.pack.add {
   'https://github.com/MunifTanjim/nui.nvim',
 }
 
-vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
+vim.keymap.set('n', '<leader>e', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
 
 require('neo-tree').setup {
   filesystem = {
     window = {
+      position = 'float',
       mappings = {
-        ['\\'] = 'close_window',
+        ['<leader>e'] = 'close_window',
       },
     },
   },
