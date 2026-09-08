663937dcab76080f7c71755e1e6ea5a26fa8d9e9
Diego Rodriguez Mancini <diego.rodriguezmancini@eso.org>
2026-09-08 16:07:01 +0200

    feat: add toggle diffview keybind

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -5,3 +5,16 @@ vim.pack.add {
 
 require('neogit').setup({})
 vim.keymap.set('n', '<leader>gg', '<cmd>Neogit<cr>', { desc = 'Open Neogit UI' })
+local function toggle_diffview()
+  local lib = require('diffview.lib')
+  local is_open = vim.iter(vim.api.nvim_list_tabpages()):any(function(tab)
+    return lib.tabpage_to_view(tab) ~= nil
+  end)
+  if is_open then
+    vim.cmd 'DiffviewClose'
+  else
+    vim.cmd 'DiffviewOpen'
+  end
+end
+
+vim.keymap.set('n', '<leader>gd', toggle_diffview, { desc = 'Toggle DiffView' })
